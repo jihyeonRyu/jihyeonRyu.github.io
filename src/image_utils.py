94e03821361b270666c5545233a023a83e092776
jihyeon.ryu <jihyeon.ryu@linecorp.com>
2021-04-08 16:56:32 +0900

    컴퓨터 구조론 하는 중

--- src/image_utils.py
+++ src/image_utils.py
@@ -2,7 +2,7 @@ import imutils
 import os
 import cv2
 
-image_path = "../assets/resource/survey/paper1"
+image_path = "../assets/resource/programming/post1"
 
 WIDTH = 500
 filenames = os.listdir(image_path)
