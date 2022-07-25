f348150d7e54fcbfcfd5fe3a7527adf16fb2351f
Jihyeon Ryu <rosie@Jihyeonui-iMac.local>
2022-07-25 15:35:11 +0900

    fix bug

--- src/image_utils.py
+++ src/image_utils.py
@@ -2,7 +2,7 @@ import imutils
 import os
 import cv2
 
-image_path = "assets/resource/ai_paper/paper26"
+image_path = "assets/resource/ai_paper/paper27"
 
 WIDTH = 500
 filenames = os.listdir(image_path)
