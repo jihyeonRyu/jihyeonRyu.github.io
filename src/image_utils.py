4a76530c66440a1ed44d3bd68b2b82c9d529b491
jihyeon.ryu <jihyeon.ryu@linecorp.com>
2023-05-03 11:25:53 +0900

    new

--- src/image_utils.py
+++ src/image_utils.py
@@ -2,7 +2,7 @@ import imutils
 import os
 import cv2
 
-image_path = "./../assets/resource/ai_paper/paper41"
+image_path = "./../assets/resource/ai_paper/paper44"
 
 WIDTH = 500
 filenames = os.listdir(image_path)
